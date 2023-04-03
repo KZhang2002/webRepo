b23578c0974cd9026c85563190c104c25684db4d
mingyao1 <106688909+mingyao1@users.noreply.github.com>
2023-04-02 23:05:12 -0500

    Add users and listings

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const mysql = require('mysql')
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'password',
+    password: 'CoolPasswordThanks',
     database: 'DBUI'
 })
 
@@ -24,33 +24,124 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.get('/user', (req, res) => {
-    const { username, password } = req.body
-    const query = `SELECT * FROM users (username, password) VALUES ('${username}', '${password}')`
+app.get('/user/auth', (req, res) => {
+    const { email, password } = req.query
+    const query = `SELECT * FROM user WHERE email='${email}' AND pass='${password}'`
     connection.query(query, (err, rows, fields) => {
         if (err) throw err
 
         console.log(rows)
+        if (rows.length == 0) {
+            res.status(401)
+            res.send("Invalid credentials")
+            return
+        }
         res.status(201)
-        token = "beans" // TODO: Token generation
+        token = generate_token(32)
+        //authtokens.set(token, rows['insertID']);
         res.send("{auth: " + token + "}")
         return
     })
-    res.status(401)
-    res.send("Invalid credentials")
 })
 
-app.post('/user', (req, res) => {
-    const { username, password } = req.body
-    const query = `INSERT INTO users (username, password) VALUES ('${username}', '${password}')`
+app.get('/user/:email', (req, res) => {
+    const email = req.query.email
+    const query = `SELECT * FROM user WHERE email='${email}'`
     connection.query(query, (err, rows, fields) => {
         if (err) throw err
 
         console.log(rows)
+        if (rows.length == 0) {
+            res.status(404)
+            res.send("We couldn't find the user you were looking for. Unfortunate, truly. Try harder I guess.")
+        }
+        const r = rows[0]
         res.status(200)
-        res.send("Successfully added user!")
+        res.json({
+            'email': r['email'],
+            'first_name': r['first_name'],
+            'last_name': r['last_name'],
+            'snowflake': r['snowflake'],
+            'strikes': r['strikes']
+        })
+        return
     })
 })
+
+app.post('/user', (req, res) => {
+    const q = req.query
+    const email = q["email"]
+    const password = q["password"]
+    const first_name = q["first_name"]
+    const last_name = q["last_name"]
+    const query = `INSERT INTO user (email, pass, first_name, last_name) VALUES ('${email}', '${password}', '${first_name}', '${last_name}')`
+    connection.query(query, (err, rows, fields) => {
+        if (err) throw err
+
+        console.log(`Successfully added user with id ${rows['insertId']}`)
+        res.status(200)
+        res.send(`Successfully added user with id ${rows['insertId']}`)
+    })
+})
+
+app.post('/listing', (req, res) => {
+    const q = req.query
+    const title = q["title"]
+    const price = q["price"]
+    const seller_email = q["seller"]
+    const desc = q["desc"]
+    const img = q["img"]
+
+    const query = `INSERT INTO listing (title, price, seller, item_description, imagelink) VALUES ('${title}', '${price}', (SELECT user.id FROM user WHERE user.email='${seller_email}'), '${desc}', '${img}')`
+    connection.query(query, (err, rows, fields) => {
+        if (err) throw err
+
+        console.log(`Successfully added listing with id ${rows['insertId']}`)
+        res.status(200)
+        res.send(`Successfully added listing with id ${rows['insertId']}`)
+    })
+})
+
+app.get('/users', (req, res) => {
+    connection.query(`SELECT * FROM user`, (err, rows, fields) => {
+        if (err) throw err
+
+        res.status(200)
+        res.send(rows)
+    })
+})
+
+app.get('/listings', (req, res) => {
+    connection.query(`SELECT * FROM listing`, (err, rows, fields) => {
+        if (err) throw err
+
+        res.status(200)
+        res.send(rows)
+    })
+})
+
+
+
+
+app.put('/users/clear', (req, res) => {
+    //console.log(req)
+    connection.query(`DELETE FROM user`, (err, rows, fields) => {
+        if (err) throw err
+
+        res.status(200)
+        res.send("Successfully cleared users!")
+    })
+})
+
+app.get('/users', (req, res) => {
+    connection.query(`SELECT * FROM user`, (err, rows, fields) => {
+        if (err) throw err
+
+        res.status(200)
+        res.send(rows)
+    })
+})
+
 
 /*app.put('/parse', (req, res) => {
     console.log(req.body)
@@ -78,25 +169,18 @@ app.post('/user', (req, res) => {
     })
 })*/
 
-/*app.get('/users', (req, res) => {
-    connection.query(`SELECT * FROM users`, (err, rows, fields) => {
-        if (err) throw err
-
-        res.status(200)
-        res.send(rows)
-    })
-})*/
-
-/*app.put('/users/clear', (req, res) => {
-    connection.query(`DELETE FROM users`, (err, rows, fields) => {
-        if (err) throw err
-
-        res.status(200)
-        res.send("Successfully cleared users!")
-    })
-})*/
-
 //start server
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+function generate_token(length){
+    //edit the token allowed characters
+    var a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".split("");
+    var b = [];  
+    for (var i=0; i<length; i++) {
+        var j = (Math.random() * (a.length-1)).toFixed(0);
+        b[i] = a[j];
+    }
+    return b.join("");
+}
