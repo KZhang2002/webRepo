9a81a8f4db90636bb91cf3a799c47fc887dce799
mingyao1 <106688909+mingyao1@users.noreply.github.com>
2023-04-12 23:25:27 -0500

    update server to encompass almost all routes

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,8 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+// Users
+
 app.get('/user/auth', (req, res) => {
     const { email, password } = req.body
     const query = `SELECT * FROM user WHERE email='${email}' AND pass='${password}'`
@@ -69,57 +71,6 @@ app.get('/user/:email', (req, res) => {
     })
 })
 
-app.post('/user', (req, res) => {
-    const {email, password, first_name, last_name} = req.body
-    const query = `INSERT INTO user (email, pass, first_name, last_name) VALUES ('${email}', '${password}', '${first_name}', '${last_name}')`
-    connection.query(query, (err, rows, fields) => {
-        if (err) throw err
-
-        console.log(`Successfully added user with id ${rows['insertId']}`)
-        res.status(200)
-        res.send(`Successfully added user with id ${rows['insertId']}`)
-    })
-})
-
-app.post('/listing', (req, res) => {
-    const q = req.body
-    const title = q["title"]
-    const price = q["price"]
-    const seller_email = q["seller"]
-    const desc = q["desc"]
-    const img = q["img"]
-
-    const query = `INSERT INTO listing (title, price, seller, item_description, imagelink) VALUES ('${title}', '${price}', (SELECT user.id FROM user WHERE user.email='${seller_email}'), '${desc}', '${img}')`
-    connection.query(query, (err, rows, fields) => {
-        if (err) throw err
-
-        console.log(`Successfully added listing with id ${rows['insertId']}`)
-        res.status(200)
-        res.send(`Successfully added listing with id ${rows['insertId']}`)
-    })
-})
-
-app.get('/users', (req, res) => {
-    connection.query(`SELECT * FROM user`, (err, rows, fields) => {
-        if (err) throw err
-
-        res.status(200)
-        res.send(rows)
-    })
-})
-
-app.get('/listings', (req, res) => {
-    connection.query(`SELECT * FROM listing`, (err, rows, fields) => {
-        if (err) throw err
-
-        res.status(200)
-        res.send(rows)
-    })
-})
-
-
-
-
 app.put('/users/clear', (req, res) => {
     //console.log(req)
     connection.query(`DELETE FROM user`, (err, rows, fields) => {
@@ -139,32 +90,108 @@ app.get('/users', (req, res) => {
     })
 })
 
-
-/*app.put('/parse', (req, res) => {
-    console.log(req.body)
-
-    try {
-        const body = req.body
-        const name = body["first"] + " " + body["last"]
-        const age = body["age"]
-        const isAdmin = body ["admin"] ? "is an admin" : "is not an admin"
-        const message = `${name} is ${age} years old and ${isAdmin}`
-        res.status(200)
-        res.send(message)
-    } catch (err) {
-        console.log(err)
-    }
-})*/
-
-/*app.get('/db', (req, res) => {
-    connection.query('SHOW TABLES', (err, rows, fields) =>{
+app.post('/user', (req, res) => {
+    const {email, password, first_name, last_name} = req.body
+    const query = `INSERT INTO user (email, pass, first_name, last_name) VALUES ('${email}', '${password}', '${first_name}', '${last_name}')`
+    connection.query(query, (err, rows, fields) => {
         if (err) throw err
 
-        console.log(rows)
+        console.log(`Successfully added user with id ${rows['insertId']}`)
+        res.status(200)
+        res.send(`Successfully added user with id ${rows['insertId']}`)
+    })
+})
+
+// Listings
+app.post('/listing', (req, res) => {
+    console.log(req.body)
+    const {title, price, seller, desc, img} = req.body
+
+    const query = `INSERT INTO listing (title, price, seller, item_description, imagelink) VALUES ('${title}', '${price}', (SELECT user.id FROM user WHERE user.email='${seller}'), '${desc}', '${img}')`
+    connection.query(query, (err, rows, fields) => {
+        if (err) throw err
+
+        console.log(`Successfully added listing with id ${rows['insertId']}`)
+        res.status(200)
+        res.send(`Successfully added listing with id ${rows['insertId']}`)
+    })
+})
+
+app.get('/listings', (req, res) => {
+    connection.query(`SELECT * FROM listing`, (err, rows, fields) => {
+        if (err) throw err
+
         res.status(200)
         res.send(rows)
     })
-})*/
+})
+
+app.get('/listing/:id', (req, res) => {
+    const { id } = req.params
+    console.log(id);
+    const query = `SELECT * FROM listing WHERE id='${ id }'`
+    connection.query(query, (err, rows, fields) => {
+        if (err) throw err
+
+        console.log(rows)
+        if (rows.length == 0) {
+            res.status(404)
+            res.send("We couldn't find the listing you were looking for. Unfortunate, truly. Try harder I guess.")
+        }
+        const r = rows[0]
+        res.status(200)
+        res.json({
+            'id': r['id'],
+            'title': r['title'],
+            'price': r['price'],
+            'created': r['created'],
+            'seller': r['seller'],
+            'item_description': r['item_description'],
+            'imagelink': r['imagelink'],
+            'approved': r['approved']
+        })
+        return
+    })
+})
+
+app.post('/listing/:id/bid', (req, res) => {
+    // TODO: Check if bid is high enough first
+    const { id } = req.params
+    const q = req.body
+    const bidder = q["bidder"] // TODO: Use auth token
+    const bid = q["bid"]
+
+    const query = `INSERT INTO bid (listing, bidder, bid) VALUES ('${id}', '${bidder}', '${bid}')`
+    connection.query(query, (err, rows, fields) => {
+        if (err) throw err
+
+        console.log(`Successfully added bid with id ${rows['insertId']}`)
+        res.status(200)
+        res.send(`Successfully added bid with id ${rows['insertId']}`)
+    })
+})
+
+// TODO: get bid
+
+app.post('/user/:id/review', (req, res) => {
+    const { id } = req.params
+    const q = req.body
+    const user = q["user"] // TODO: Use auth token
+    const review = q["bid"]
+
+    const query = `INSERT INTO review (user, review, seller) VALUES ('${user}', '${review}', '${id}')`
+    connection.query(query, (err, rows, fields) => {
+        if (err) throw err
+
+        console.log(`Successfully added bid with id ${rows['insertId']}`)
+        res.status(200)
+        res.send(`Successfully added bid with id ${rows['insertId']}`)
+    })
+})
+
+// TODO: get reviews
+
+// TODO: Tags
 
 //start server
 app.listen(port, () => {
